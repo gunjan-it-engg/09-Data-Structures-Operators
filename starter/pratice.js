b4d9e686875f91127d34a9ef1afe7176c4fb203a
gunjanshrimali <gunjanshrimali1234@gmail.com>
2021-08-09 17:53:48 +0530

    intial commit

---
+++ starter/pratice.js
@@ -0,0 +1,43 @@
+const game = {
+    team1 : 'gunjan',
+    team2 : 'shrimali',
+    players :[
+        [
+            'sanjay',
+            'piyush',
+            'ritesh',
+            'lavina',
+            'jatin',
+            'deepak',
+            'vinod',
+        ],
+        [
+            'aditi',
+            'usha',
+            'bhumika',
+            'pooja',
+            'dhruv',
+            'abhishek',
+            'rahul',
+        ],
+        [
+            'kashish',
+            'mayank',
+            'ronak',
+            'ajay',
+            'komal',
+            'kapil',
+            'shiv',
+        ],
+    ],
+    score : '4:0',
+    scored :['usha','sanjay','dhruv','pooja'],
+    date: 'Aug 9th, 2037',
+    odds:{
+        team1:1.23,
+        x:3.25,
+        team2:6.5,
+    },
+};
+
+const [players1 , players2] = game.players
