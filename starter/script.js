674b6b182601452e20e8ae42825b56225b083d25
gunjanshrimali <gunjanshrimali1234@gmail.com>
2021-08-10 12:36:21 +0530

    optional chaining topic - apply on obj , array , methods

--- starter/script.js
+++ starter/script.js
@@ -3,7 +3,7 @@
 // Data needed for a later exercise
 const flights ='_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 // This is the object enhancement!
-const weekDays = ['mon','thu','wed'];
+const weekDays = ['mon','tue','wed','thur','fri'];
 const openingHours = {
     [weekDays[0]]: {
       open: 12,
@@ -35,3 +35,31 @@ console.log(restaurant);
 
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 for (const item of menu.entries()) console.log(item);
+
+
+// optional chaining in object topic.
+// This will be without chaining type example .
+if (restaurant.openingHours && restaurant.openingHours.mon) 
+console.log(`resturent opening hours is ${restaurant.openingHours.mon.open}!`);
+
+// with optional chaining 
+// so without optinal chaining if we writing condition (resturent.openingHours.mon.open) we
+// getting an Type error in console but if we using this chaining operator if got undefined .
+console.log(restaurant.openingHours.sat?.open);
+console.log(restaurant.openingHours?.mon?.open);
+// optinal chaining real example
+const days = ['mon','tue','wed','thur','fri','sat','sun'];
+for (const day of days){
+    console.log(day);
+    const open = restaurant.openingHours[day]?.open || 'closed';
+    console.log(`on ${day},we open at ${open}`);
+}
+// Methods optinal chaining.
+console.log(restaurant.order?.(0,1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0,1)??'Method Does not exist');
+
+// optional chaining works on array.
+const users=[
+    {name:'jonas', email:'gunjan12@gmail.com',}
+];
+console.log(users[0]?.name ?? 'user array is empty');
